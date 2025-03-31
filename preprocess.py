40577028085ccff1a1fac4c94044ce5d8aeddec1
beetleberries <93026704+beetleberries@users.noreply.github.com>
2025-03-31 11:10:22 +1100

    more stuff

--- preprocess.py
+++ preprocess.py
@@ -12,6 +12,9 @@ print(tensorflow.keras.__version__)
 print(tf.__version__)
 print(tf.keras.__version__) 
 from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+
 
 def readcsv(filepath):
     df = pd.read_csv(filepath, comment='#', header=0, dtype={"Timestamp": float, "Arbitration_ID": str, "Data": str, "Class": str})
@@ -39,15 +42,18 @@ def readcsv(filepath):
 
 
 def compile_model(SEQ_LENGTH, featurelength): 
-    
+    #force gpu
+    physical_devices = tf.config.list_physical_devices("GPU")
+    if physical_devices:
+        tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
     # Build LSTM Model
     model = Sequential([
-        LSTM(64, return_sequences=True, input_shape=(SEQ_LENGTH, featurelength)),
+        LSTM(64, return_sequences=True, activation="relu", input_shape=(SEQ_LENGTH, featurelength)),
         Dropout(0.2),
-        LSTM(32, return_sequences=False),
-        Dropout(0.2),
-        Dense(16, activation='relu'),
+        LSTM(64, return_sequences=False, activation="relu"),
+        Dropout(0.5),
+        Dense(32, activation='relu'),
         Dense(1, activation='sigmoid')  # Binary classification
     ])
 
@@ -62,18 +68,20 @@ def create_sequences(data, target_column, seq_length=10):
     return X, y
 
 def main():
-    df = readcsv("./Car_Hacking_Challenge_Dataset_rev20Mar2021/0_Preliminary/0_Training/Pre_train_D_0.csv")
+    df = readcsv("./Car_Hacking_Challenge_Dataset_rev20Mar2021/0_Preliminary/0_Training/Pre_train_D_1.csv")
     
     print(f"The amount of anomalies is {df['Class'].sum() / len(df) * 100:.2f}% currently")
 
-    SEQ_LENGTH = 10
+    SEQ_LENGTH = 50
     features = ["Timestamp", "Arbitration_ID", "Data"]
 
     print(df.columns)
     X, y = create_sequences(df[features], df["Class"], seq_length=SEQ_LENGTH)
+
+
     print("splitting")
     # Split into training and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=False)
 
     # Reshape for LSTM (samples, timesteps, features)
     X_train = np.reshape(X_train, (X_train.shape[0], SEQ_LENGTH, len(features)))
@@ -85,8 +93,9 @@ def main():
 
     print("training")
     early_stop = EarlyStopping(monitor="val_loss", patience=2, restore_best_weights=True)
-
-    model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=50, callbacks=[early_stop])
+    checkpoint = ModelCheckpoint(
+    "models/lstm_most_recent_best_model.weights.h5", monitor="val_loss", save_best_only=True, save_weights_only=True)
+    model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=50, callbacks=[early_stop, checkpoint])
 
     # Evaluate the model
     loss, accuracy = model.evaluate(X_test, y_test)
